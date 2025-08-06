1cfc1411d937028b1535813fa7763e195f59e02a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-05 22:47:06 -0300

    mais exercicios

---
+++ src/ValorTotal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ValorTotal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int produto, cont1, resposta;
+        double preco, totalpreco=0;
+        System.out.println("digite a quantidad de produto");
+        produto= sc.nextInt();
+        for (cont1=1;cont1<=produto;cont1++){
+            System.out.println("digite o preço");
+            preco= sc.nextDouble();
+            totalpreco=totalpreco+preco;
+
+        }
+        System.out.println("o preço ficou "+totalpreco);
+        System.out.println("digite 1 para pagar à vista ou 2 para parcelar em duas vezes");
+        resposta= sc.nextInt();
+        switch (resposta){
+            case 1->{
+                totalpreco=totalpreco-(totalpreco*10/100);
+                System.out.println("o preço vai será de "+totalpreco);
+            }
+            default -> {
+                totalpreco=(totalpreco+(totalpreco*15.5/100))/2;
+                System.out.println("o preço de cada parcela será de "+totalpreco);
+            }
+        }
+    }
+}
---
+++ src/exercicioComPara.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercicioComPara {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int valor, cont=1, resultado;
+        System.out.println("digite um valor");
+        valor= sc.nextInt();
+        if (valor<=0){
+            System.out.println("o valor tem que ser maior que zero");
+        }
+        else {
+        for (;cont<=valor;cont++) {
+            resultado=valor%cont;
+            if (resultado==0){
+                System.out.println(+cont);
+            }
+
+
+        }
+        }
+    }
+}
